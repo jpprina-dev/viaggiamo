2771e46e4fb87790dd2a616fb23362e738e1df2b
Juan Prina <jpprina@gmail.com>
2026-01-10 15:08:22 +0000

    update step to 5 min, some browsers did not change

--- frontend/src/features/trip-create/components/StepDateTime.tsx
+++ frontend/src/features/trip-create/components/StepDateTime.tsx
@@ -59,6 +59,7 @@ export function StepDateTime({ register, errors }: StepDateTimeProps) {
             <Clock className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
             <input
               type="time"
+              step="300"
               {...register('departureTime')}
               className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary-500 focus:border-transparent transition-all"
             />
